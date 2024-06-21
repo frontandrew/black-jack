968477190cfd1d1da680672b7d78d2617d4c36f4
AndrewBubnov <jackblack.nt@gmail.com>
2024-06-21 14:48:22 +0500

    deps: add plain css transpiler

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,6 +5,9 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/test.{ts,tsx}'],
+  transform: {
+    "^.+\\.css$": "jest-transform-css"
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
