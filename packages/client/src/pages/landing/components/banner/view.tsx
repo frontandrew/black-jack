397a8f8883d86a88b8e13ce01c1de62539b3245d
AndrewBubnov <jackblack.nt@gmail.com>
2024-06-30 19:42:28 +0500

    chore: separate landing banner component

---
+++ packages/client/src/pages/landing/components/banner/view.tsx
@@ -0,0 +1,74 @@
+import { Button, Grid, Typography, useTheme } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+import { LandBack } from '../../../../assets/imgs'
+
+export const LandingBanner = () => {
+  const navigate = useNavigate()
+  const { spacing, palette } = useTheme()
+
+  const defaultBackgroundColor = palette.background.default
+  const contrastBackgroundColor = palette.getContrastText(
+    defaultBackgroundColor
+  )
+  const contrastTextColor = palette.getContrastText(contrastBackgroundColor)
+
+  return (
+    <Grid container padding={spacing(2)}>
+      <Grid
+        container
+        bgcolor={contrastBackgroundColor}
+        color={contrastTextColor}
+        borderRadius={'60px'}
+        position={'relative'}
+        overflow={'hidden'}
+        width={'100%'}>
+        <Grid
+          item
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'flex-end'}
+          width={'100%'}
+          height={'100%'}
+          position={'absolute'}>
+          <img height={'200%'} src={LandBack} />
+        </Grid>
+
+        <Grid
+          item
+          display={'flex'}
+          flexDirection={'column'}
+          gap={spacing(8)}
+          width={'100%'}
+          padding={spacing(10, 16)}>
+          <Grid
+            container
+            display={'flex'}
+            flexDirection={'column'}
+            sx={{ mixBlendMode: 'difference' }}>
+            <Typography variant="h1" noWrap>
+              Try your luck
+            </Typography>
+            <Typography variant="h4" noWrap>
+              in world most popular card game
+            </Typography>
+          </Grid>
+
+          <Grid container width={300}>
+            <Button
+              fullWidth
+              size={'large'}
+              variant={'contained'}
+              onClick={() => {
+                navigate('/sign-up')
+              }}>
+              <Typography variant={'h6'} noWrap>
+                PUSH TO WIN
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
--- packages/client/src/pages/landing/view.tsx
+++ packages/client/src/pages/landing/view.tsx
@@ -1,6 +1,7 @@
 import { Button, Grid, Typography, useTheme } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import { LandBack } from '../../assets/imgs'
+
+import { LandingBanner } from './components'
 
 // import './style.css';
 
@@ -60,57 +61,7 @@ export const LandingPage = () => {
         className={'landing-content'}
         pt={spacing(1)}
         overflow={'hidden auto'}>
-        <Grid container className={'banner'} padding={spacing(2)}>
-          <Grid
-            container
-            className={'banner-container'}
-            bgcolor={contrastBackgroundColor}
-            color={contrastTextColor}
-            borderRadius={'60px'}
-            position={'relative'}
-            overflow={'hidden'}
-            width={'100%'}>
-            <Grid
-              item
-              display={'flex'}
-              alignItems={'center'}
-              justifyContent={'flex-end'}
-              width={'100%'}
-              height={'100%'}
-              position={'absolute'}>
-              <img height={'200%'} src={LandBack} />
-            </Grid>
-            <Grid
-              item
-              display={'flex'}
-              flexDirection={'column'}
-              gap={spacing(8)}
-              width={'100%'}
-              padding={spacing(10, 16)}>
-              <Grid container display={'flex'} flexDirection={'column'}>
-                <Typography variant="h1" noWrap>
-                  Try your luck
-                </Typography>
-                <Typography variant="h4" noWrap>
-                  in world most popular card game
-                </Typography>
-              </Grid>
-              <Grid container width={'20%'}>
-                <Button
-                  fullWidth
-                  size={'large'}
-                  variant="contained"
-                  onClick={() => {
-                    navigate('/sign-up')
-                  }}>
-                  <Typography variant="h6" noWrap>
-                    PUSH TO WIN
-                  </Typography>
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        <LandingBanner />
 
         <Grid className={'timeline'}>
           <Grid
