169521dbbff26f889e10c6f56e471a51cd232946
Andrew <56526926+frontandrew@users.noreply.github.com>
2024-07-03 11:57:17 +0500

    BLA-19: landing page (#40)
    merge: BLA-19-landing into sprint_5

---
+++ packages/client/src/pages/landing/components/header/view.tsx
@@ -0,0 +1,39 @@
+import { Button, Grid, Typography, useTheme } from '@mui/material'
+import { FC } from 'react'
+import { useNavigate } from 'react-router'
+
+export const LandingHeader: FC = () => {
+  const navigate = useNavigate()
+  const { spacing, palette } = useTheme()
+
+  return (
+    <Grid
+      item
+      component={'header'}
+      display={'flex'}
+      alignItems={'center'}
+      width={'100%'}
+      gap={spacing(2)}
+      padding={spacing(4, 2)}
+      borderBottom={1}
+      borderColor={palette.divider}>
+      <Typography variant="h4" flexGrow={1}>
+        Black Jack
+      </Typography>
+      <Button
+        variant={'text'}
+        onClick={() => {
+          navigate('/sign-in')
+        }}>
+        SIGN IN
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate('/sign-up')
+        }}>
+        SIGN UP
+      </Button>
+    </Grid>
+  )
+}
