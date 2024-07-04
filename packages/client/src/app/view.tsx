0487edc03c5487d03661fe7b38622466f8366adc
TentakleM1 <124381778+TentakleM1@users.noreply.github.com>
2024-07-04 23:54:38 +0300

    fix: deleted hook useeffect

--- packages/client/src/app/view.tsx
+++ packages/client/src/app/view.tsx
@@ -8,16 +8,6 @@ import './style.css'
 import { router } from './router'
 
 export function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return (
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
