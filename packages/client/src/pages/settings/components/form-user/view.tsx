33ae470da382ea886d2bc2976f9a427db983eeb1
AndrewBubnov <jackblack.nt@gmail.com>
2024-07-04 18:13:35 +0500

    feat: create user form

--- packages/client/src/pages/settings/components/form-user/view.tsx
+++ packages/client/src/pages/settings/components/form-user/view.tsx
@@ -1,46 +1,103 @@
-import { useTheme, Button } from '@mui/material'
+import { useTheme, Button, Grid } from '@mui/material'
 import { useForm } from 'react-final-form-hooks'
+
 import type { FC } from 'react'
 
-import { FieldText } from '../../../../shared/components'
+import { FieldText } from 'components'
+
+/* TODO: need to use UserType */
+type UserType = object
+
+const user: Array = {
+  email: 'some@email.it',
+  login: 'Somelogin',
+  first_name: 'First',
+  second_name: 'Last',
+  display_name: 'Nick',
+  phone: '+45134543345',
+}
 
 const config = {
   validateOnBlur: true,
-  onSubmit: values => {
-    console.table(values)
+  onSubmit: (formValues: UserType) => {
+    console.table(formValues)
   },
-  initialValues: { firstName: 'bbb', lastName: 'rrr' },
+  initialValues: user,
 }
 
+/* TODO: need to use shared/validator */
 const validator = (val: string) => {
   console.log(`%c VALID[value: ${val}]:`, 'color: white; background-color: red')
   return val !== 'aaa' ? '' : 'Ivalid value'
 }
 
 export const FormUser: FC = () => {
-  const { palette, spacing } = useTheme()
+  const { spacing } = useTheme()
   const { form, handleSubmit } = useForm(config)
 
-  // console.log(`FORM:`, { form });
-
   return (
-    <form onSubmit={handleSubmit}>
+    <Grid
+      component={'form'}
+      width={'100%'}
+      height={'min-content'}
+      onSubmit={handleSubmit}>
       <FieldText
         form={form}
-        name={'lastName'}
-        label={'Last Name'}
-        validator={validator}
-      />
-      <FieldText
-        form={form}
-        name={'firstName'}
-        label={'First Name'}
+        name={'email'}
+        label={'Email'}
         validator={validator}
         required
       />
-      <Button type={'submit'} disabled={form.getState().hasValidationErrors}>
-        SUBMIT
-      </Button>
-    </form>
+      <FieldText
+        form={form}
+        name={'login'}
+        label={'Login'}
+        validator={validator}
+        required
+      />
+      <FieldText
+        form={form}
+        name={'first_name'}
+        label={'First name'}
+        validator={validator}
+        required
+      />
+      <FieldText
+        form={form}
+        name={'second_name'}
+        label={'Last name'}
+        validator={validator}
+        required
+      />
+      <FieldText
+        form={form}
+        name={'display_name'}
+        label={'Nickname'}
+        validator={validator}
+        required
+      />
+      <FieldText
+        form={form}
+        name={'phone'}
+        label={'Phone'}
+        validator={validator}
+        required
+      />
+      <Grid container justifyContent={'flex-end'} gap={spacing(2)}>
+        <Button
+          type={'submit'}
+          variant={'contained'}
+          disabled={form.getState().hasValidationErrors}>
+          SUBMIT
+        </Button>
+        <Button
+          variant={'outlined'}
+          onClick={() => {
+            form.reset()
+          }}>
+          CANCEL
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
