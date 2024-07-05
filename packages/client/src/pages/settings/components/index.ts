3f5861050ae029c8db5687756b4fd81b39145735
AndrewBubnov <jackblack.nt@gmail.com>
2024-07-05 17:40:14 +0500

    chore: implement css on mui api

--- packages/client/src/pages/settings/components/index.ts
+++ packages/client/src/pages/settings/components/index.ts
@@ -1,1 +1,2 @@
 export { FormUser } from './form-user'
+export { CardUser } from './card-user'
--- packages/client/src/pages/settings/view.tsx
+++ packages/client/src/pages/settings/view.tsx
@@ -1,21 +1,14 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  Grid,
-  Typography,
-  useTheme,
-} from '@mui/material'
+import { Box, Divider, Grid, useMediaQuery, useTheme } from '@mui/material'
 
 import type { FC } from 'react'
 
-import { SomeAvatar } from 'images'
-import { FormUser } from './components'
-import './style.css'
+import { CardUser, FormUser } from './components'
 
 export const SettingsPage: FC = () => {
-  const { palette, spacing } = useTheme()
+  const { spacing } = useTheme()
+  const isLandscape = useMediaQuery('(min-width:900px)')
+
+  const contentDirection = isLandscape ? 'row' : 'column'
 
   return (
     <Grid
@@ -25,79 +18,20 @@ export const SettingsPage: FC = () => {
       alignItems={'center'}
       width={'100%'}
       height={'100%'}>
-      <Box className={'settings-content'} maxWidth={1080} padding={spacing(4)}>
+      <Box
+        maxWidth={1080}
+        display={'flex'}
+        boxSizing={'border-box'}
+        flexDirection={contentDirection}
+        padding={spacing(4)}>
         <Grid item padding={spacing(4, 8)} minWidth={400}>
-          <Grid
-            className={'settings-user-card'}
-            container
-            width={'100%'}
-            height={'100%'}
-            flexDirection={'column'}
-            alignItems={'center'}
-            gap={spacing(6)}>
-            <Avatar src={SomeAvatar} sx={{ width: 200, height: 200 }} />
-            <Grid container flexDirection={'column'} alignItems={'center'}>
-              <Typography variant={'h4'} fontWeight={800}>
-                Nickname
-              </Typography>
-              <Typography variant={'overline'} color={'GrayText'}>
-                Firstname Secondname
-              </Typography>
-            </Grid>
-            <Grid
-              container
-              flexDirection={'column'}
-              alignItems={'center'}
-              gap={spacing(6)}
-              flexGrow={1}>
-              <Grid container flexDirection={'column'} alignItems={'center'}>
-                <Grid container justifyContent={'space-between'}>
-                  <Typography variant={'h5'}>PLAYED GAMES:</Typography>
-                  <Typography
-                    variant={'h5'}
-                    fontWeight={600}
-                    sx={{ color: palette.primary.light }}>
-                    145
-                  </Typography>
-                </Grid>
-
-                <Grid container justifyContent={'space-between'}>
-                  <Typography variant={'h5'}>MAX CASH:</Typography>
-                  <Typography
-                    variant={'h5'}
-                    fontWeight={600}
-                    sx={{ color: palette.success.light }}>
-                    $ 23423
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid
-              container
-              className={'settings-user-card__actions'}
-              width={'max-content'}
-              alignItems={'flex-end'}
-              justifyContent={'space-between'}
-              gap={spacing(2)}>
-              <Button variant={'outlined'}>change avatar</Button>
-              <Button variant={'contained'} color={'error'}>
-                sign out
-              </Button>
-            </Grid>
-          </Grid>
+          <CardUser />
         </Grid>
 
         <Divider orientation={'vertical'} flexItem />
 
-        <Grid item minWidth={400}>
-          <Grid
-            className={'settings-user-form'}
-            item
-            width={'100%'}
-            padding={spacing(4, 8)}>
-            <FormUser />
-          </Grid>
+        <Grid item padding={spacing(4, 8)} minWidth={400}>
+          <FormUser />
         </Grid>
       </Box>
     </Grid>
