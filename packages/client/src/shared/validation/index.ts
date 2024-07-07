d92d59f9092040785a07e7988e894bbe5dfdda4f
TentakleM1 <tentakelme7@gmail.com>
2024-07-07 10:03:51 +0300

    feat: made a phone check, and export out module

--- packages/client/src/shared/validation/index.ts
+++ packages/client/src/shared/validation/index.ts
@@ -1,6 +1,14 @@
 import { Validators } from './type'
+import { email } from './validators/email/email'
 import { login } from './validators/login/login'
+import { name } from './validators/name/name'
+import { password } from './validators/password/password'
+import { phone } from './validators/phone/phone'
 
 export const validators: Validators = {
   login,
+  password,
+  phone,
+  email,
+  name,
 }
