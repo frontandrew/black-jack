b9877c452b8c75ee7fe178c3d1c5cf086208b68b
AndrewBubnov <jackblack.nt@gmail.com>
2024-07-08 12:56:11 +0500

    chore: learers layouting

--- packages/client/src/pages/leader/view.tsx
+++ packages/client/src/pages/leader/view.tsx
@@ -1,21 +1,33 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 
-import './style.css'
 import { useNavigate } from 'react-router-dom'
 
 export const LeaderPage = () => {
   const navigate = useNavigate()
 
   return (
-    <Box className="leader-page">
-      <Typography variant="h3">Leaderboard</Typography>
-      <Button
-        variant="contained"
-        onClick={() => {
-          navigate('/sign-in')
-        }}>
-        SIGN IN
-      </Button>
-    </Box>
+    <Grid
+      container
+      width={'100%'}
+      height={'100%'}
+      justifyContent={'center'}
+      alignItems={'center'}>
+      <Grid
+        container
+        maxWidth={'1080px'}
+        overflow={'auto hidden'}
+        flexDirection={'column'}
+        justifyContent={'center'}
+        alignItems={'center'}>
+        <Typography variant="h3">Leaderboard</Typography>
+        <Button
+          variant="contained"
+          onClick={() => {
+            navigate('/sign-in')
+          }}>
+          SIGN IN
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
