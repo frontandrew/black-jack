14ef1bbef6ca7c4594d8f8dbe0c8615be18dae1b
TentakleM1 <tentakelme7@gmail.com>
2024-07-09 09:51:56 +0300

    feat: integrated validation into registration, made the blur effect

--- packages/client/src/pages/reg/view.tsx
+++ packages/client/src/pages/reg/view.tsx
@@ -9,15 +9,129 @@ import {
 } from '@mui/material'
 import './style.css'
 import { useNavigate } from 'react-router'
+import { FC, useState } from 'react'
+import { validators } from '../../shared/validation/index'
 
-export const RegPage = () => {
+type HandlerForms = (value: Record<string, string>) => void
+
+export const RegPage: FC = () => {
   const { spacing } = useTheme()
   const navigate = useNavigate()
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [second, setSecond] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
 
+  const [errorLogin, setErrorLogin] = useState(false)
+  const [errorPassword, setErrorPassword] = useState(false)
+  const [errorName, setErrorName] = useState(false)
+  const [errorSecond, setErrorSecond] = useState(false)
+  const [errorEmail, setErrorEmail] = useState(false)
+  const [errorPhone, setErrorPhone] = useState(false)
+
+  const [halperLogin, setHalperLogin] = useState('')
+  const [halperPassword, setHalperPassword] = useState('')
+  const [halperName, setHalperName] = useState('')
+  const [halperSecond, setHalperSecond] = useState('')
+  const [halperEmail, setHalperEmail] = useState('')
+  const [halperPhone, setHalperPhone] = useState('')
+
+  const handlerForms: HandlerForms = value => {
+    const loginErrors = validators.login(value.login)
+    const passwordErrors = validators.password(value.password)
+    const nameErrors = validators.name(value.name)
+    const secondErrors = validators.name(value.second)
+    const emailErrors = validators.email(value.email)
+    const phoneErrors = validators.phone(value.phone)
+
+    if (loginErrors.length > 0) {
+      setErrorLogin(true)
+      setHalperLogin(loginErrors[0])
+    }
+
+    if (nameErrors.length > 0) {
+      setErrorName(true)
+      setHalperName(nameErrors[0])
+    }
+
+    if (secondErrors.length > 0) {
+      setErrorSecond(true)
+      setHalperSecond(secondErrors[0])
+    }
+
+    if (emailErrors.length > 0) {
+      setErrorEmail(true)
+      setHalperEmail(emailErrors[0])
+    }
+
+    if (phoneErrors.length > 0) {
+      setErrorPhone(true)
+      setHalperPhone(phoneErrors[0])
+    }
+
+    if (passwordErrors.length > 0) {
+      setErrorPassword(true)
+      setHalperPassword(passwordErrors[0])
+    }
+
+    if (passwordErrors.length === 0) {
+      setHalperPassword('')
+      setErrorPassword(false)
+    }
+
+    if (nameErrors.length === 0) {
+      setErrorName(false)
+      setHalperName('')
+    }
+
+    if (secondErrors.length === 0) {
+      setErrorSecond(false)
+      setHalperSecond('')
+    }
+
+    if (emailErrors.length === 0) {
+      setErrorEmail(false)
+      setHalperEmail('')
+    }
+
+    if (phoneErrors.length === 0) {
+      setErrorPhone(false)
+      setHalperPhone('')
+    }
+
+    if (loginErrors.length === 0) {
+      setHalperLogin('')
+      setErrorLogin(false)
+    }
+
+    if (
+      passwordErrors.length === 0 &&
+      loginErrors.length === 0 &&
+      nameErrors.length === 0 &&
+      secondErrors.length === 0 &&
+      emailErrors.length === 0 &&
+      phoneErrors.length === 0
+    ) {
+      console.log(value)
+    }
+  }
   return (
     <Box className="login-page">
       <Paper elevation={3} square={false}>
-        <form>
+        <form
+          onSubmit={e => {
+            e.preventDefault() // Предотвращаем перезагрузку страницы
+            handlerForms({
+              login: login,
+              password: password,
+              email: email,
+              name: name,
+              second_name: second,
+              phone: phone,
+            })
+          }}>
           <Grid
             direction="column"
             display="flex"
@@ -27,17 +141,93 @@ export const RegPage = () => {
               Register
             </Typography>
 
-            <TextField label="Name" type="text" size="small" />
+            <TextField
+              error={errorName}
+              label="Name"
+              type="text"
+              size="small"
+              helperText={halperName}
+              onChange={e => setName(e.target.value)}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ name: name })
+                },
+              }}
+            />
 
-            <TextField label="Second Name" type="text" size="small" />
+            <TextField
+              error={errorSecond}
+              label="Second Name"
+              type="text"
+              size="small"
+              helperText={halperSecond}
+              onChange={e => setSecond(e.target.value)}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ second: second })
+                },
+              }}
+            />
 
-            <TextField label="Login" type="text" size="small" />
+            <TextField
+              error={errorLogin}
+              name="login"
+              label="Login"
+              type="text"
+              size="small"
+              helperText={halperLogin}
+              onChange={e => setLogin(e.target.value)}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ login: login })
+                },
+              }}
+            />
 
-            <TextField label="Email" type="text" size="small" />
+            <TextField
+              error={errorEmail}
+              label="Email"
+              type="text"
+              size="small"
+              helperText={halperEmail}
+              onChange={e => setEmail(e.target.value)}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ email: email })
+                },
+              }}
+            />
 
-            <TextField label="password" type="password" size="small" />
+            <TextField
+              error={errorPassword}
+              name="password"
+              label="password"
+              type="password"
+              size="small"
+              helperText={halperPassword}
+              onChange={e => {
+                setPassword(e.target.value)
+              }}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ password: password })
+                },
+              }}
+            />
 
-            <TextField label="Phone" type="text" size="small" />
+            <TextField
+              error={errorPhone}
+              label="Phone"
+              type="text"
+              size="small"
+              helperText={halperPhone}
+              onChange={e => setPhone(e.target.value)}
+              inputProps={{
+                onBlur: () => {
+                  handlerForms({ phone: phone })
+                },
+              }}
+            />
 
             <Button type="submit" variant="contained">
               SIGN UP
