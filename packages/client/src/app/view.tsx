6a6b74eacef529a53dbc2c7063c2b8226f9a0585
TentakleM1 <tentakelme7@gmail.com>
2024-07-09 21:29:32 +0300

    merge bla-20 to sprint 6

--- packages/client/src/app/view.tsx
+++ packages/client/src/app/view.tsx
@@ -1,55 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useEffect } from 'react'
+import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material'
-
-import {
-  ErrorPage,
-  FinishPage,
-  ForumPage,
-  GamePage,
-  LandingPage,
-  LeaderPage,
-  LoginPage,
-  RegPage,
-  SettingsPage,
-  StartPage,
-} from 'pages'
-import { theme } from 'theme'
+import { theme } from '../shared/theme'
 
 import './style.css'
-import { Provider } from 'react-redux'
-import { store } from './store'
+import { router } from './router'
 
 export function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="sign-in" element={<LoginPage />} />
-            <Route path="sign-up" element={<RegPage />} />
-            <Route path="settings" element={<SettingsPage />} />
-            <Route path="start" element={<StartPage />} />
-            <Route path="game" element={<GamePage />} />
-            <Route path="finish" element={<FinishPage />} />
-            <Route path="leaderboard" element={<LeaderPage />} />
-            <Route path="forum" element={<ForumPage />} />
-            <Route path="forum/:forumId" element={<ForumPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
