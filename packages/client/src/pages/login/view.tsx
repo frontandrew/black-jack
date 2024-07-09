32c0caa73f74da08e40365351add3b3d2ede01dd
TentakleM1 <tentakelme7@gmail.com>
2024-07-09 22:49:43 +0300

    feat: maked reg page

--- packages/client/src/pages/login/view.tsx
+++ packages/client/src/pages/login/view.tsx
@@ -1,30 +1,48 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  useTheme,
+} from '@mui/material'
 import { useNavigate } from 'react-router'
+import { FC } from 'react'
 
 import './style.css'
 
-export const LoginPage = () => {
+export const LoginPage: FC = () => {
+  const { spacing } = useTheme()
   const navigate = useNavigate()
 
   return (
     <Box className="login-page">
-      <Typography variant="h3">Login Page</Typography>
-      <Grid direction="column" display="flex" gap="1em">
-        <Button
-          variant="contained"
-          onClick={() => {
-            navigate('/sign-in')
-          }}>
-          SIGN IN
-        </Button>
-        <Button
-          variant="text"
-          onClick={() => {
-            navigate('/sign-up')
-          }}>
-          sign up
-        </Button>
-      </Grid>
+      <Paper elevation={3} square={false}>
+        <form>
+          <Grid
+            direction="column"
+            display="flex"
+            gap="2em"
+            padding={spacing(2, 6)}>
+            <Typography variant="h3" align="center" margin={spacing(2)}>
+              Login
+            </Typography>
+            <TextField label="Login" type="text" size="small" />
+            <TextField label="password" type="password" size="small" />
+            <Button type="submit" variant="contained">
+              SIGN IN
+            </Button>
+            <Button
+              variant="text"
+              onClick={() => {
+                navigate('/sign-up')
+              }}>
+              sign up
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
     </Box>
   )
 }
