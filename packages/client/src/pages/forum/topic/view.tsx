a891c359e9e11432ef49e5d4c0afb8f26b38e19d
Oleg_Ratanov <o.ratanov@yandex.ru>
2024-07-13 19:08:57 +0300

    fix: added some style for forum

--- packages/client/src/pages/forum/topic/view.tsx
+++ packages/client/src/pages/forum/topic/view.tsx
@@ -1,8 +1,8 @@
 import React, { useState, FC } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../app/store'
-import { useParams } from 'react-router-dom'
-import { Box, TextField, Button, Typography } from '@mui/material'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Box, TextField, Button, Typography, Container } from '@mui/material'
 import { addComment } from '../../../features/forum/model'
 
 export const TopicPage: FC = () => {
@@ -12,40 +12,72 @@ export const TopicPage: FC = () => {
   const topic = useSelector((state: RootState) =>
     state.topics.topics.find(t => t.id === parseInt(id, 10))
   )
+  const navigate = useNavigate()
 
-  if (!topic) return <div>Topic not found</div>
+  if (!topic)
+    return (
+      <Container maxWidth="md" className="mt-5 text-center">
+        <Box>
+          <Typography variant="h4">Topic not found</Typography>
+          <Button
+            onClick={() => navigate('/forum')}
+            variant="contained"
+            color="primary"
+            className="mt-5">
+            Go to forum
+          </Button>
+        </Box>
+      </Container>
+    )
 
   const handleAddComment = () => {
+    if (!comment.length) return
     const newComment = { id: Date.now(), content: comment }
     dispatch(addComment({ topicId: topic.id, comment: newComment }))
     setComment('')
   }
 
   return (
-    <Box>
-      <Typography variant="h4">{topic.title}</Typography>
-      <Typography variant="body1" paragraph>
-        {topic.content}
-      </Typography>
+    <Container maxWidth="md" className="mt-5">
       <Box>
-        <TextField
-          label="Add a comment"
-          value={comment}
-          onChange={e => setComment(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <Button onClick={handleAddComment} variant="contained" color="primary">
-          Add Comment
-        </Button>
+        <div className="flex justify-between mb-5">
+          <Typography variant="h4">{topic.title}</Typography>
+          <Button
+            onClick={() => navigate('/forum')}
+            variant="contained"
+            color="primary">
+            Back to forum
+          </Button>
+        </div>
+        <Typography paragraph className="mt-5">
+          {topic.content}
+        </Typography>
+        <Box>
+          <TextField
+            label="Add a comment"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            onClick={handleAddComment}
+            variant="contained"
+            color="primary">
+            Add Comment
+          </Button>
+        </Box>
+        <Box>
+          {topic.comments.length > 0 && (
+            <div className="mt-4">User comments:</div>
+          )}
+          {topic.comments.map((cmt, index) => (
+            <Typography key={cmt.id} className="mt-2 ml-4">
+              {++index}: {cmt.content}
+            </Typography>
+          ))}
+        </Box>
       </Box>
-      <Box>
-        {topic.comments.map(cmt => (
-          <Typography key={cmt.id} variant="body2">
-            {cmt.content}
-          </Typography>
-        ))}
-      </Box>
-    </Box>
+    </Container>
   )
 }
--- packages/client/src/pages/forum/view.tsx
+++ packages/client/src/pages/forum/view.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
-import { Typography, List, ListItem, ListItemText, Button } from '@mui/material'
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+  Container,
+  Box,
+} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
 interface ForumPageProps {
@@ -13,23 +21,28 @@ export const ForumPage: FC<ForumPageProps> = ({ onAddTopicClick }) => {
   const topics = useSelector((state: RootState) => state.topics.topics)
 
   return (
-    <div>
-      <div className="flex">
-        <Typography variant="h4">Blackjack Forum</Typography>
-        <Button onClick={onAddTopicClick} variant="contained" color="primary">
-          Add New Topic
-        </Button>
-      </div>
-      <List>
-        {topics.map(topic => (
-          <ListItem
-            button
-            key={topic.id}
-            onClick={() => navigate(`/forum/${topic.id}`)}>
-            <ListItemText primary={topic.title} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <Container maxWidth="md" className="mt-5">
+      <Box>
+        <div className="flex justify-between">
+          <Typography variant="h4">Blackjack Forum</Typography>
+          <Button onClick={onAddTopicClick} variant="contained" color="primary">
+            Add New Topic
+          </Button>
+        </div>
+        <List>
+          {topics.length === 0 && (
+            <div className="mt-4">No topic has been created on the forum</div>
+          )}
+          {topics.map(topic => (
+            <ListItem
+              button
+              key={topic.id}
+              onClick={() => navigate(`/forum/${topic.id}`)}>
+              <ListItemText primary={topic.title} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Container>
   )
 }
