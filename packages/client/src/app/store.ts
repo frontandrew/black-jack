a0ce83c8e70d1ac11398552e49957b657fdcc3a3
AndrewBubnov <jackblack.nt@gmail.com>
2024-07-17 17:51:06 +0500

    chore: connect to store

--- packages/client/src/app/store.ts
+++ packages/client/src/app/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import gameReducer from 'features/game/model'
+import { authSlice, userSlice } from 'services'
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
+    auth: authSlice.reducer,
+    user: userSlice.reducer,
   },
 })
 
