3455521c669520902bfdb323cf68b0604bde7f59
AndrewBubnov <jackblack.nt@gmail.com>
2024-07-17 17:54:47 +0500

    merge: sprint_6 into BLA-55-user-auth

--- packages/client/src/app/store.ts
+++ packages/client/src/app/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import gameReducer from 'features/game/model'
 import { authSlice, userSlice } from 'services'
+import topicsReducer from 'features/forum/model'
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
     auth: authSlice.reducer,
     user: userSlice.reducer,
+    topics: topicsReducer,
   },
 })
 
