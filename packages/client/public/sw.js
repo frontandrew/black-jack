4da751bf558aa44f9111986d1f847617901a27d0
TentakleM1 <tentakelme7@gmail.com>
2024-07-17 05:25:17 +0300

    fix: ServiceWorker

--- packages/client/public/sw.js
+++ packages/client/public/sw.js
@@ -34,27 +34,31 @@ self.addEventListener('activate', async event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      if (response) {
-        return response
-      }
+      if (response) response
 
       const fetchRequest = event.request.clone()
 
-      return fetch(fetchRequest).then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+      return fetch(fetchRequest)
+        .then(response => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== 'basic'
+          ) {
+            return response
+          }
+
+          const responseToCache = response.clone()
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache)
+          })
+
           return response
-        }
-
-        const responseToCache = response.clone()
-
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseToCache)
         })
-
-        return response.catch(error => {
+        .catch(error => {
           console.log(error)
         })
-      })
     })
   )
 })
