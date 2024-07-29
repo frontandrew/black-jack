7edd46cc77c12ae48ac15f790c754a1586e17038
Andrew <56526926+frontandrew@users.noreply.github.com>
2024-07-30 01:30:35 +0500

    merge: logger into sprint_6
    feat: add redux logger

--- packages/client/src/shared/store/store.ts
+++ packages/client/src/shared/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import gameReducer from 'features/game/model'
 import topicsReducer from 'features/forum/model'
+import logger from 'redux-logger'
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
     topics: topicsReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV === 'development',
 })
 
 export type RootState = ReturnType<typeof store.getState>
