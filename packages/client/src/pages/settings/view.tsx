8ce51bff7cb0adb7f73e1650c644ded53f8d6c84
AndrewBubnov <jackblack.nt@gmail.com>
2024-08-21 23:40:18 +0500

    layout: add header page/settings

--- packages/client/src/pages/settings/view.tsx
+++ packages/client/src/pages/settings/view.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet'
 import { Box, Divider, Grid, useMediaQuery, useTheme } from '@mui/material'
+import { AppHeader } from 'components'
+
 import { CardUser, FormUser } from './components'
 
 export const SettingsPage: React.FC = () => {
@@ -11,30 +13,39 @@ export const SettingsPage: React.FC = () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Settings</title>
+        <title>Profile</title>
         <meta name="description" content="Settings" />
       </Helmet>
       <Grid
         container
-        overflow={'hidden auto'}
-        justifyContent={'center'}
-        alignItems={'center'}
+        display={'flex'}
+        flexDirection={'column'}
+        flexWrap={'nowrap'}
         width={'100%'}
         height={'100%'}>
-        <Box
-          maxWidth={1080}
-          display={'flex'}
-          boxSizing={'border-box'}
-          flexDirection={contentDirection}
-          padding={spacing(4)}>
-          <Grid item padding={spacing(4, 8)} minWidth={400}>
-            <CardUser />
-          </Grid>
-          <Divider orientation={'vertical'} flexItem />
-          <Grid item padding={spacing(4, 8)}>
-            <FormUser />
-          </Grid>
-        </Box>
+        <AppHeader />
+        <Grid
+          container
+          overflow={'hidden auto'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          width={'100%'}
+          height={'100%'}>
+          <Box
+            maxWidth={1080}
+            display={'flex'}
+            boxSizing={'border-box'}
+            flexDirection={contentDirection}
+            padding={spacing(4)}>
+            <Grid item padding={spacing(4, 8)} minWidth={400}>
+              <CardUser />
+            </Grid>
+            <Divider orientation={'vertical'} flexItem />
+            <Grid item padding={spacing(4, 8)}>
+              <FormUser />
+            </Grid>
+          </Box>
+        </Grid>
       </Grid>
     </>
   )
