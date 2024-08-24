007c2c4c3acd134eb62a4590429e1c1f5ffd19ac
AndrewBubnov <jackblack.nt@gmail.com>
2024-08-24 20:46:53 +0500

    chore: create themes object

--- packages/client/src/shared/themes/index.ts
+++ packages/client/src/shared/themes/index.ts
@@ -1,3 +1,8 @@
-export * from './dark'
-export * from './light'
+import { darkTheme } from './dark'
+import { lightTheme } from './light'
+
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+}
 export * from './style-cache'
